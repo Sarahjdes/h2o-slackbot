6859120e4016111e644bfa3badef3ebca97e1cc6
sarahjdes <sjdesrochers@challengeu.com>
2017-01-21 14:47:37 -0500

    H2O bot now spawns!

---
+++ h2o.js
@@ -0,0 +1,14 @@
+var Botkit = require('./lib/Botkit.js');
+
+if (!process.env.token) {
+    console.log('Erreur : Dude, t\'as oublié de spécifier ton token!');
+    process.exit(1);
+}
+
+var controller = Botkit.slackbot({
+    debug: true,
+});
+
+controller.spawn({
+    token : process.env.token
+}).startRTM();
